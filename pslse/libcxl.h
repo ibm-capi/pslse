c7d7838b41267fc5b4bfac9c262dcd8076845c71
Kirk Morrow <kmorrow@us.ibm.com>
2015-03-26 15:16:13 -0500

    Fixed cxl_event_pending() name mismatch with .c function

--- pslse/libcxl.h
+++ pslse/libcxl.h
@@ -166,7 +166,7 @@ enum cxl_image {
 /*
  * Events
  */
-int cxl_pending_event(struct cxl_afu_h *afu);
+int cxl_event_pending(struct cxl_afu_h *afu);
 int cxl_read_event(struct cxl_afu_h *afu, struct cxl_event *event);
 /*int cxl_read_expected_event(struct cxl_afu_h *afu, struct cxl_event *event,
 			    __u32 type, __u16 irq);*/
